944e989fce53f29067f8bf0e35c5067007994e7b
yeahokpal <113112958+yeahokpal@users.noreply.github.com>
2024-01-18 00:45:46 -0500

    Added Map Layout Variants

---
+++ MiscAssets/MapLayout.gd
@@ -0,0 +1,12 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(2,6):
+		set_layer_enabled(i, false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
